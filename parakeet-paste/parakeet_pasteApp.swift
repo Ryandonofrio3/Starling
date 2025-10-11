ce4916db36ab41688c30cdb744eba23872ed469d
Ryan D'Onofrio <97113725+Ryandonofrio3@users.noreply.github.com>
2025-10-11 10:06:28 -0700

    Initial Commit

---
+++ parakeet-paste/parakeet_pasteApp.swift
@@ -0,0 +1,17 @@
+//
+//  parakeet_pasteApp.swift
+//  parakeet-paste
+//
+//  Created by Ryan D'Onofrio on 10/11/25.
+//
+
+import SwiftUI
+
+@main
+struct parakeet_pasteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
